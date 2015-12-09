25ae7d87a4457fa302cebfa7ea41a68f99de381b
guojianghui <514230657@qq.com>
2015-12-09 18:45:18 +0800

    gdf

---
+++ wfd/wfd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wfd
+//
+//  Created by gjh on 15/12/9.
+//  Copyright © 2015年 gjh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wfd/wfd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wfd
+//
+//  Created by gjh on 15/12/9.
+//  Copyright © 2015年 gjh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
